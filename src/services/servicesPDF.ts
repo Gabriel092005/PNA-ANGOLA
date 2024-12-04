9fb7d3f9ff1c36409d49528b122a03f5e3bd4dad
Gabriel092005 <gabrielcavalamanuel@gmail.com>
2024-12-04 09:31:00 +0000

    adicionando cores

--- src/services/servicesPDF.ts
+++ src/services/servicesPDF.ts
@@ -1,23 +1,43 @@
-import { PDFDocument, StandardFonts } from 'pdf-lib'; // Certifique-se de importar StandardFonts
-
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'; // Importando o rgb para cores
 
 export class PdfService {
   async generatePdf(title: string, content: string): Promise<Buffer> {
     const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage([600, 400]);
-    const { width, height } = page.getSize();
+    const page = pdfDoc.addPage([600, 1000]);
+    const {height } = page.getSize();
 
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    const textSize = 12;
+    const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
+   
+    const titleSize = 18;
+    const contentSize = 12;
+
+
+    const titleColor = rgb(0.2, 0.5, 0.8); // Cor azul
+    const contentColor = rgb(0, 0, 0); // Cor preta
+
+
+    page.drawText(title, {
+      x: 50,
+      y: height - 50, 
+      size: titleSize,
+      font: boldFont,
+      color: titleColor,
+    });
+
 
     page.drawText(content, {
       x: 50,
-      y: height - 100,
-      size: textSize,
-      font,
+      y: height - 100, 
+      size: contentSize,
+      font: font,
+      color: contentColor,
+      lineHeight: 15, 
     });
 
-    const pdfBytes = await pdfDoc.save(); // O PDF é gerado de forma assíncrona
-    return Buffer.from(pdfBytes); // Converte os bytes para um Buffer
+
+    const pdfBytes = await pdfDoc.save();
+    return Buffer.from(pdfBytes); 
   }
 }
